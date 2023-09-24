4973d53b1224f579994e3be9e3ad5f2a31451b33
Alexandre Viala <alexandre.viala@outlook.com>
2023-09-23 23:03:24 -0400

    ✨ Add graphical Vertex structure

--- libs/internal/PeRender/CPeGraphicalVertex.h
+++ libs/internal/PeRender/CPeGraphicalVertex.h
@@ -8,19 +8,24 @@
 #include <vulkan/vulkan.h>
 #include <array>
 
-struct Vertex {
-    glm::vec3 pos;
-    glm::vec3 color;
-    glm::vec2 texCoord;
+namespace vk {
+    struct Vertex {
+        glm::vec3 pos;
+        glm::vec3 color;
+        glm::vec2 texCoord;
 
-    static VkVertexInputBindingDescription getBindingDescription();
+        static VkVertexInputBindingDescription getBindingDescription();
 
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
-};
+        static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
+
+        bool operator==(const Vertex& other) const;
+    };
+}
+
 
 namespace std {
-    template<> struct hash<Vertex> {
-        size_t operator()(Vertex const& vertex) const {
+    template<> struct hash<vk::Vertex> {
+        size_t operator()(vk::Vertex const& vertex) const {
             return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
         }
     };
