0db0d8d765380c1a2322962735b40929519ed8bb
Alexandre Viala <alexandre.viala@outlook.com>
2023-10-13 12:18:26 -0400

    ✅ Add test in main + update cmake

--- main.cpp
+++ main.cpp
@@ -10,12 +10,14 @@
 
 #include "libs/internal/PeRender/CPeVulkanRenderer.h"
 #include "libs/internal/PeRender/CPeGraphicalObjects.h"
+#include "libs/internal/PeRender/SphereMesh.h"
 
 #include <stdio.h>          // printf, fprintf
 #include <stdlib.h>
 
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+#include <cmath>
 
 // Main code
 int main(int, char**)
@@ -32,13 +34,24 @@ int main(int, char**)
     imguiRenderer.SetupInterface();
     renderer.init(window, &imguiRenderer);
 
-    vk::ModelWatcher model1(renderer, "models/viking_room.obj");
+    //vk::ModelWatcher model1(renderer, "models/viking_room.obj");
+    vk::SphereMesh model2(renderer);
+
+    //vk::ModelWatcher model2(renderer, "models/viking_room.obj");
+
+    float i = 0.f;
+    model2.SetScale(0.5f);
+    model2.SetPos(0, -1, -1);
 
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
+        //model1.SetPos(0, 0, std::sin(i));
+        //model2.SetPos(std::sin(i), 0, 0);
+        //model2.SetScale(std::abs(std::sin(i)) * 2);
         glfwPollEvents();
         renderer.drawFrame();
+        i += 0.1f;
     }
     
 
