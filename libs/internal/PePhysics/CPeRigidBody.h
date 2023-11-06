6c311d4e33ff89702c22383e2be91ca84fb082c4
Alexandre Viala <alexandre.viala@outlook.com>
2023-11-06 10:45:43 -0500

    ✨ Add rigid body basic body

--- libs/internal/PePhysics/CPeRigidBody.h
+++ libs/internal/PePhysics/CPeRigidBody.h
@@ -10,7 +10,26 @@ namespace pecore = engine::core;
 namespace engine {
 	namespace physics {
         class CPeRigidBody : public pecore::CPeComponent {
+        private:
+            float inverseMass;
+            float linearDampling;
+            pemaths::CPeVector3 position;
+            pemaths::CPeVector3 velocity;
 
+            pemaths::CPeQuaternion orientation;
+
+            pemaths::CPeVector3 angularVelocity;
+
+            pemaths::CPeMatrix4 transformMatrix;
+        public:
+            /**
+             * @brief .Modify position, orientation & velocities
+             */
+            void Integrate(float p_timeStep);
+            /**
+             * @brief Call each frame to compute transformMatrix & normalize the orientation.
+             */
+            void ComputeDerivedData();
         };
     }
 }
