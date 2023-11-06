c634cecd531f033fcc79c1066d60155bbda9dff4
Alexandre Viala <alexandre.viala@outlook.com>
2023-11-06 10:15:15 -0500

    ♻️ Add alias on namespaces

--- libs/internal/PePhysics/CPeRigidBody.h
+++ libs/internal/PePhysics/CPeRigidBody.h
@@ -4,11 +4,14 @@
 #include "../PeMaths/PeMaths.h"
 #include "../PeEngineCore/PeEngineCore.h"
 
+namespace pemaths = engine::maths;
+namespace pecore = engine::core;
+
 namespace engine {
 	namespace physics {
         class CPeRigidBody : public pecore::CPeComponent {
 
-        }
+        };
     }
 }
 
