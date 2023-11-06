537a05cc4944586060742e1f668b77f0110cfd64
Alexandre Viala <alexandre.viala@outlook.com>
2023-11-06 09:42:52 -0500

    ✨ Initialize RigidBody class

---
+++ libs/internal/PePhysics/CPeRigidBody.cpp
@@ -0,0 +1,1 @@
+#include "CPeRigidBody.h"
---
+++ libs/internal/PePhysics/CPeRigidBody.h
@@ -0,0 +1,15 @@
+#ifndef CPERIGIDBODY_CPERIGIDBODY_H
+#define CPERIGIDBODY_CPERIGIDBODY_H
+
+#include "../PeMaths/PeMaths.h"
+#include "../PeEngineCore/PeEngineCore.h"
+
+namespace engine {
+	namespace physics {
+        class CPeRigidBody : public pecore::CPeComponent {
+
+        }
+    }
+}
+
+#endif
