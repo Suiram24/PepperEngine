2978f5033bbbbafa4f96bb354710af814643a190
Alexandre Viala <alexandre.viala@outlook.com>
2023-11-09 23:08:43 -0500

    🎨 Implement spring force for particles + bodies

--- libs/internal/PePhysics/EngineForces/CPeForceSpring.h
+++ libs/internal/PePhysics/EngineForces/CPeForceSpring.h
@@ -9,14 +9,19 @@ namespace engine {
 	namespace physics {
 		class CPeForceSpring : public CPeForce {
 		private:
-			CPeParticle* m_other;
+			pemaths::CPeVector3 m_bodyAnchor;
+
+			engine::physics::CPePhysicalObject* m_other;
+			pemaths::CPeVector3 m_otherBodyAnchor;
+
 			float m_k;
 			float m_restLength;
-
 		public:
 			CPeForceSpring()
 				: CPeForce()
 				, m_other(nullptr)
+				, m_bodyAnchor(0,0,0)
+				, m_otherBodyAnchor(0,0,0)
 				, m_k(0)
 				, m_restLength(0)
 			{
@@ -24,8 +29,10 @@ namespace engine {
 			}
 
 			void Initialise(CPeParticle* p_other, float p_k, float p_restLength);
+			void Initialise(CPePhysicalObject* p_other, pemaths::CPeVector3 p_otherLocalAnchor, float p_k, float p_restLength);
 
-			void Compute(CPeParticle& p_particule, double p_timeStep) const;
+			void Compute(CPeParticle& p_particle, double p_timeStep) const;
+			void Compute(CPeRigidBody& p_rigidBody, double p_timeStep) const;
 		};
 	}
 }
