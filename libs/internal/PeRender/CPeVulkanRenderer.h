dfc53c324d7d845484387bd0d14ada98dacd27ad
Alexandre Viala <alexandre.viala@outlook.com>
2023-11-13 15:23:27 -0500

    ⚰️ Delete old loop

--- libs/internal/PeRender/CPeVulkanRenderer.h
+++ libs/internal/PeRender/CPeVulkanRenderer.h
@@ -278,20 +278,5 @@ namespace vk {
             void* pUserData
         );
     };
-    /*
-    int main() {
-        HelloTriangleApplication app;
-
-        try {
-            app.run();
-        }
-        catch (const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-            return EXIT_FAILURE;
-        }
-
-        return EXIT_SUCCESS;
-    }
-    */
 }
 #endif
