c49929559083e9a8d0c94e9c6d7e0411881c52ff
Marius Bozane <42440457+Suiram24@users.noreply.github.com>
2023-11-19 23:37:37 -0500

    Added new pendulum demo

--- main.cpp
+++ main.cpp
@@ -11,13 +11,17 @@
 
 //#include "demo/DemoCustomGameMode.h"
 #include "demo/DemoPhase3.h"
+#include "demo/DemoDoublePendulum.h"
 #include "CPeGameManager.h"
 
 // Main code
 int main(int, char**)
 {
 
-    engine::CPeGameMode* myGameMode = &pedemo::DemoPhase3();
+    //engine::CPeGameMode* myGameMode = &pedemo::DemoPhase3();
+    engine::CPeGameMode* myGameMode = &pedemo::DemoDoublePendulum();
+
+
     engine::CPeGameManager::getInstance().SetGameMode(myGameMode);
     engine::CPeGameManager::getInstance().StartGame();
 
