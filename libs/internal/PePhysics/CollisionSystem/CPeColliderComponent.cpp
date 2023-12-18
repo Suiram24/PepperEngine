f63e4df2781522f1b0ed044bc53d018015e3c2f3
Benoît M <103281274+BenoitMiege@users.noreply.github.com>
2023-12-18 10:19:59 -0500

    Fix bug imported from master

--- libs/internal/PePhysics/CollisionSystem/CPeColliderComponent.cpp
+++ libs/internal/PePhysics/CollisionSystem/CPeColliderComponent.cpp
@@ -2,26 +2,7 @@
 
 namespace engine {
 	namespace physics {
-		void CPeColliderComponent::Initialise(pecore::CPeEntity* p_owner, double p_radius)
-		{
-			m_isActive = true;
-			m_owner = p_owner;
-
-			m_owner->AddComponent(this);
-		}
-		void CPeColliderComponent::SetActive(bool value)
-		{
-			if (!value)
-			{
-				m_owner = nullptr;
-			}
-		}
-
-		bool CPeColliderComponent::isActive() const
-		{
-			return false;
-		}
-
+	
 		CPeSpherePrimitiveShape& CPeColliderComponent::GetGlobalVolume() {
 			return *m_enclosingShape;
 		}
